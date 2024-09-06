737bc2cc968da82de5bf554dbae311d2089c7bc3
WESLEY VIEIRA MARTINS DE ANDRADE <15102938681@campusrp-lab.riopomba.ifsudestemg.edu.br>
2024-09-06 08:06:59 -0300

    nothing

---
+++ LibraryProject/src/main/java/trabalho/libraryproject/view/JDialogUser.java
@@ -0,0 +1,394 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
+ */
+package trabalho.libraryproject.view;
+
+import java.util.List;
+import javax.swing.JOptionPane;
+import trabalho.libraryproject.connection.SQLiteConnector;
+import trabalho.libraryproject.controller.UserController;
+import trabalho.libraryproject.model.entities.User;
+import trabalho.libraryproject.view.TableModel.TMCadUser;
+
+/**
+ *
+ * @author Windows
+ */
+public class JDialogUser extends javax.swing.JDialog {
+
+    private boolean editando;
+    private String oldCpf;
+    private UserController userController;
+    private User userEditing;
+    public JDialogUser(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        setLocationRelativeTo(parent);
+        
+         this.editando = false;
+        this.oldCpf = "";
+        this.userEditing = new User();
+        
+        //IDao BookJ = new BookJSONSerializer("ListagemAutores.json");
+        //SQLiteConnector conexao = new SQLiteConnector("dataBase.sqlite");
+        //IDao userDao = new UserDAOFile(conexao.getConnection()); 
+        //this.userController = new UserController(userDao);
+        
+        this.habilitarCampos(false);
+        this.limparCampos();
+        
+        this.atualizarTabela();
+        
+    }
+    
+   
+    public void habilitarCampos(boolean flag){
+       edtName.setEnabled(flag);
+        edtCPF.setEnabled(flag);
+        edtAge.setEnabled(flag);
+       
+        
+    }
+    
+    public void limparCampos(){
+        edtName.setText("");
+        edtCPF.setText("");
+        edtAge.setText("");
+        
+    }
+    
+    public void objetoParaCampos(User u){
+        edtName.setText(u.getName());
+        edtCPF.setText(u.getCpf());
+        edtAge.setText(u.getAge());
+        
+    }
+
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        lblUserRegistration = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        btnNew = new javax.swing.JButton();
+        btnCancel = new javax.swing.JButton();
+        btnEdit = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        btnSave = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        lblName = new javax.swing.JLabel();
+        edtName = new javax.swing.JTextField();
+        lblCPF = new javax.swing.JLabel();
+        edtCPF = new javax.swing.JTextField();
+        lblAge = new javax.swing.JLabel();
+        edtAge = new javax.swing.JTextField();
+        jPanel4 = new javax.swing.JPanel();
+        lblISBN = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        grdUser = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(51, 204, 255));
+
+        lblUserRegistration.setFont(new java.awt.Font("Lucida Calligraphy", 1, 24)); // NOI18N
+        lblUserRegistration.setForeground(new java.awt.Color(255, 255, 255));
+        lblUserRegistration.setText("User Registration ");
+        jPanel1.add(lblUserRegistration);
+
+        jPanel2.setBackground(new java.awt.Color(102, 255, 255));
+
+        btnNew.setBackground(new java.awt.Color(204, 204, 255));
+        btnNew.setForeground(new java.awt.Color(255, 255, 255));
+        btnNew.setText("NEW");
+        btnNew.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNewActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnNew);
+
+        btnCancel.setBackground(new java.awt.Color(204, 204, 255));
+        btnCancel.setForeground(new java.awt.Color(255, 255, 255));
+        btnCancel.setText("CANCEL");
+        btnCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnCancel);
+
+        btnEdit.setBackground(new java.awt.Color(204, 204, 255));
+        btnEdit.setForeground(new java.awt.Color(255, 255, 255));
+        btnEdit.setText("EDIT");
+        btnEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnEdit);
+
+        btnDelete.setBackground(new java.awt.Color(204, 204, 255));
+        btnDelete.setForeground(new java.awt.Color(255, 255, 255));
+        btnDelete.setText("DELETE");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnDelete);
+
+        btnSave.setBackground(new java.awt.Color(204, 204, 255));
+        btnSave.setForeground(new java.awt.Color(255, 255, 255));
+        btnSave.setText("SAVE");
+        btnSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSaveActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnSave);
+
+        jPanel3.setBackground(new java.awt.Color(102, 204, 255));
+
+        lblName.setForeground(new java.awt.Color(0, 0, 0));
+        lblName.setText("Name : ");
+
+        edtName.setForeground(new java.awt.Color(0, 0, 0));
+
+        lblCPF.setForeground(new java.awt.Color(0, 0, 0));
+        lblCPF.setText("CPF : ");
+
+        edtCPF.setForeground(new java.awt.Color(0, 0, 0));
+
+        lblAge.setForeground(new java.awt.Color(0, 0, 0));
+        lblAge.setText("Age : ");
+
+        edtAge.setForeground(new java.awt.Color(0, 0, 0));
+
+        jPanel4.setBackground(new java.awt.Color(153, 153, 255));
+        jPanel4.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel4.setForeground(new java.awt.Color(204, 204, 255));
+
+        lblISBN.setForeground(new java.awt.Color(0, 0, 0));
+        lblISBN.setText("Book : ");
+
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
+        jButton1.setForeground(new java.awt.Color(0, 0, 0));
+        jButton1.setText("Select");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblISBN)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblISBN)
+                    .addComponent(jButton1))
+                .addContainerGap(10, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(lblName)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(edtName, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(lblCPF)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(edtCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(32, Short.MAX_VALUE))
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(lblAge)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(edtAge, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblName)
+                    .addComponent(edtName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblCPF)
+                    .addComponent(edtCPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblAge)
+                            .addComponent(edtAge, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(60, 60, 60))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(22, 22, 22))))
+        );
+
+        grdUser.setBackground(new java.awt.Color(204, 204, 255));
+        grdUser.setForeground(new java.awt.Color(0, 0, 0));
+        grdUser.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(grdUser);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+       this.limparCampos();
+       this.habilitarCampos(false);
+       this.editando = false;
+    }//GEN-LAST:event_btnCancelActionPerformed
+
+    private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
+         if (this.editando == true){
+          this.userController.update(oldCpf, edtName.getText(), edtCPF.getText(),edtAge.getText() );
+       }
+       else{
+          this.userController.add(edtName.getText(), edtCPF.getText(), edtAge.getText());
+       }
+       this.limparCampos();
+       this.habilitarCampos(false);
+       this.editando = false;
+       
+       this.atualizarTabela();
+    }//GEN-LAST:event_btnSaveActionPerformed
+
+    private void btnNewActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewActionPerformed
+       this.habilitarCampos(true);
+       this.limparCampos();
+       this.editando = false;
+    }//GEN-LAST:event_btnNewActionPerformed
+
+    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
+        String NomeEscolhido = JOptionPane.showInputDialog("Enter the name a user you want to EDIT:", "");
+
+        this.userEditing = (User) this.userController.find(NomeEscolhido);
+        
+        if(userEditing == null){
+            JOptionPane.showMessageDialog(this,"There is no user with this name.");
+            this.editando = false;
+        }else{
+            this.limparCampos();
+            this.habilitarCampos(true);
+            
+            this.objetoParaCampos(userEditing);
+            this.editando = true;
+            this.oldCpf= userEditing.getName();
+            this.atualizarTabela();
+        }
+    }//GEN-LAST:event_btnEditActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+       String NameEscolhido = JOptionPane.showInputDialog("Enter the name a user you want to delete :", "");
+        this.userEditing = (User)this.userController.find(NameEscolhido);
+        if(userEditing == null){
+            JOptionPane.showMessageDialog(this,"There is no user with this name.");
+            this.editando = false;
+        }else{
+            this.userController.remove(NameEscolhido);
+            JOptionPane.showMessageDialog(this,"Deletion done successfully!");
+        }
+        this.atualizarTabela();
+    }
+
+     public void atualizarTabela() {
+        List<User> lista = this.userController.list();
+        TMCadUser tmcadUser = new TMCadUser(lista);
+        grdUser.setModel(tmcadUser);
+                                      
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        //JDialogSelectBook tela = new JDialogSelectBook(this, true);
+        //tela.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnEdit;
+    private javax.swing.JButton btnNew;
+    private javax.swing.JButton btnSave;
+    private javax.swing.JTextField edtAge;
+    private javax.swing.JTextField edtCPF;
+    private javax.swing.JTextField edtName;
+    private javax.swing.JTable grdUser;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblAge;
+    private javax.swing.JLabel lblCPF;
+    private javax.swing.JLabel lblISBN;
+    private javax.swing.JLabel lblName;
+    private javax.swing.JLabel lblUserRegistration;
+    // End of variables declaration//GEN-END:variables
+}
